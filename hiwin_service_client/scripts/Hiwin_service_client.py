626d9b4b3fe5b9a623f24a93595d35f7fcd68e12
errrr0501 <luis.done0501@gmail.com>
2023-03-23 22:35:16 +0800

    try to trun service_client use Robotcommand.srv

--- hiwin_service_client/scripts/Hiwin_service_client.py
+++ hiwin_service_client/scripts/Hiwin_service_client.py
@@ -3,8 +3,12 @@
 
 import rclpy
 from rclpy.node import Node
+from rclpy.task import Future
+import time
+from threading import Thread
 
-from hiwin_interfaces.srv import Hiwinmodbus
+from hiwin_interfaces.srv import RobotCommand
+from geometry_msgs.msg import Twist
 '''
     DO(int DO_Num, int x)                                                         # 1 -> on ; 0 -> off                                          
     HOME(int state)                                                               # 1 RUN
@@ -14,92 +18,207 @@ from hiwin_interfaces.srv import Hiwinmodbus
     JOG(int joint,int dir)
 '''
 
+DEFAULT_VELOCITY = 10
+DEFAULT_ACCELERATION = 10
+
+VACUUM_PIN = 3
+
+PHOTO_POSE = [0.00, 0.00, 0.00, 0.00, -90.00, 0.00]
+# OBJECT_POSE = [20.00, 0.00, 0.00, 0.00, -90.00, 0.00]
+OBJECT_POSE = [-67.517, 361.753, 293.500, 180.00, 0.00, 100.572]
+PLACE_POSE = [-20.00, 0.00, 0.00, 0.00, -90.00, 0.00]
+
+# only for example as we don't use yolo here
+# assume NUM_OBJECTS=5, then this process will loop 5 times
+NUM_OBJECTS = 5
 
 class HiwinmodbusClient(Node):
 
     def __init__(self):
         super().__init__('hiwinmodbus_client')
-        self.cli = self.create_client(Hiwinmodbus, 'hiwinmodbus_service')
-        while not self.cli.wait_for_service(timeout_sec=1.0):
+        self.hiwin_client = self.create_client(RobotCommand, 'hiwinmodbus_service')
+
+    def _wait_for_future_done(self, future: Future, timeout=-1):
+        time_start = time.time()
+        while not future.done():
+            time.sleep(0.01)
+            if timeout > 0 and time.time() - time_start > timeout:
+                self.get_logger().error('Wait for service timeout!')
+                return False
+        print("+++++++++++++++++=")
+        return True
+
+    def call_hiwin(self, request):
+        while not self.hiwin_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
-        self.command_req = Hiwinmodbus.Request()
+        future = self.hiwin_client.call_async(request)
+        # if self._wait_for_future_done(future):
+        #     res = future.result()
+        #     print(res)
+        # else:
+        #     res = None
+        res = future.result().arm_state
+        return res
+        # self.future = self.hiwin_client.call_async(request)
+        # rclpy.spin_until_future_complete(self, self.future)
+        # return self.future.result()
 
-    def send_command_callback(self):
-        self.future = self.cli.call_async(self.command_req)
-        rclpy.spin_until_future_complete(self, self.future)
-        return self.future.result()
-    
-    # def Holding_command(self, holding):
+    def generate_robot_request(
+            self, 
+            holding=True,
+            cmd_mode=RobotCommand.Request.PTP,
+            cmd_type=RobotCommand.Request.POSE_CMD,
+            velocity=DEFAULT_VELOCITY,
+            acceleration=DEFAULT_ACCELERATION,
+            digital_output_pin=0,
+            digital_output_cmd=RobotCommand.Request.DIGITAL_OFF,
+            poses=Twist(),
+            joints=[float('inf')]*6,
+            circ_s=[],
+            circ_end=[],
+            jog_joint=6,
+            jog_dir=0
+            ):
+        request = RobotCommand.Request()
+        request.digital_output_pin = digital_output_pin
+        request.digital_output_cmd = digital_output_cmd
+        request.acceleration = acceleration
+        request.jog_joint = jog_joint
+        request.velocity = velocity
+        request.cmd_mode = cmd_mode
+        request.cmd_type = cmd_type
+        request.circ_end = circ_end
+        request.jog_dir = jog_dir
+        request.holding = holding
+        request.joints = joints
+        request.circ_s = circ_s
+        request.pose = poses
 
-    #     self.command_req.holding = holding
+        return request
 
     def call_motor_excite(self, holding=False):
 
-        self.command_req.mode  ='Excite'
-        self.command_req.holding = holding
-        return self.send_command_callback() 
+        req = self.generate_robot_request(
+            cmd_mode=RobotCommand.Request.EXCITE,
+            holding=holding
+        )
+        return self.call_hiwin(req) 
 
-    def call_ptp(self, type, vel, acc, pose, holding=False):
+    def call_ptp(self, pose, cmd_type=RobotCommand.Request.POSE_CMD, 
+    holding=False,velocity=DEFAULT_VELOCITY, acceleration=DEFAULT_ACCELERATION):
         
-        self.command_req.mode  ='PTP'
-        self.command_req.type  = type 
-        self.command_req.vel   = vel
-        self.command_req.acc   = acc
-        self.command_req.pose = pose
-        self.command_req.holding = holding
-        return self.send_command_callback()
+        # print(pose)
+        # print(cmd_type)
+        # print(holding)
+        # print(velocity)
+        # print(acceleration)
+        if cmd_type == 0:
+            print("-------------")
+            req = self.generate_robot_request(
+                cmd_mode=RobotCommand.Request.PTP,
+                cmd_type=cmd_type,
+                velocity=velocity,
+                acceleration=acceleration, 
+                joints=pose,
+                holding=holding
+            )
+        if cmd_type == 1:
+            poses = Twist()
+            [poses.linear.x, poses.linear.y, poses.linear.z] = pose[0:3]
+            [poses.angular.x, poses.angular.y, poses.angular.z] = pose[3:6]
+            req = self.generate_robot_request(
+                cmd_mode=RobotCommand.Request.PTP,
+                cmd_type=cmd_type,
+                velocity=velocity,
+                acceleration=acceleration, 
+                poses=poses,
+                holding=holding
+            )
+        # print(req)
+        return self.call_hiwin(req) 
 
-    def call_lin(self, type, vel, acc, pose, holding=False):
+    def call_line(self, pose, cmd_type=RobotCommand.Request.POSE_CMD, 
+    holding=False,  velocity=DEFAULT_VELOCITY, acceleration=DEFAULT_ACCELERATION):
 
-        self.command_req.mode  ='LIN'
-        self.command_req.type = type 
-        self.command_req.vel  = vel
-        self.command_req.acc  = acc
-        self.command_req.pose  = pose
-        self.command_req.holding = holding
-        return self.send_command_callback()
-
-
-    def call_circ(self, vel, acc, circ_s, circ_end, holding=False):
-
-        self.command_req.mode  ='CIRC'
-        self.command_req.vel      = vel
-        self.command_req.acc      = acc
-        self.command_req.circ_s   = circ_s
-        self.command_req.circ_end = circ_end
-        self.command_req.holding = holding
-        return self.send_command_callback()
-
-
-    def call_do(self, digital_output, onoff, holding=False):
-
-        self.command_req.mode  ='DO'
-        self.command_req.digital_output = digital_output
-        self.command_req.onoff          = onoff
-        self.command_req.holding = holding
-        return self.send_command_callback()
+        if cmd_type == 0:
+            req = self.generate_robot_request(
+                cmd_mode=RobotCommand.Request.LINE,
+                cmd_type=cmd_type,
+                velocity=velocity,
+                acceleration=acceleration, 
+                joints=pose,
+                holding=holding
+            )
+        if cmd_type == 1:
+            poses = Twist()
+            [poses.linear.x, poses.linear.y, poses.linear.z] = pose[0:3]
+            [poses.angular.x, poses.angular.y, poses.angular.z] = pose[3:6]
+            req = self.generate_robot_request(
+                cmd_mode=RobotCommand.Request.LINE,
+                cmd_type=cmd_type,
+                velocity=velocity,
+                acceleration=acceleration, 
+                poses=poses,
+                holding=holding
+            )
+        return self.call_hiwin(req) 
 
 
-    def call_home(self, holding=False):
-        self.command_req.mode  ='HOME'
-        self.command_req.type = 0
-        self.command_req.holding = holding
-        return self.send_command_callback()
+    def call_circ(self, circ_s, circ_end,holding=False, 
+    velocity=DEFAULT_VELOCITY, acceleration=DEFAULT_ACCELERATION):
+
+        req = self.generate_robot_request(
+            cmd_mode=RobotCommand.Request.CIRC,
+            velocity=velocity,
+            acceleration=acceleration, 
+            circ_s=circ_s,
+            circ_end=circ_end,
+            holding=holding
+        )
+        return self.call_hiwin(req)
 
 
-    def call_jog(self, joint, dir, holding=False):
+    def call_do(self, digital_output_pin, digital_output_cmd=RobotCommand.Request.DIGITAL_OFF, holding=False):
 
-        self.command_req.mode  ='JOG'
-        self.command_req.joint = joint
-        self.command_req.dir   = dir
-        self.command_req.holding = holding
-        return self.send_command_callback()
+        req = self.generate_robot_request(
+            cmd_mode=RobotCommand.Request.DIGITAL_OUTPUT,
+            digital_output_cmd=digital_output_cmd,
+            digital_output_pin=digital_output_pin,
+            holding=holding
+        )
+        return self.call_hiwin(req)
+
+
+
+    def call_home(self, holding=True):
+        req = self.generate_robot_request(
+            cmd_mode=RobotCommand.Request.HOME,
+            holding=holding
+        )
+        return self.call_hiwin(req)
+
+
+    def call_jog(self, jog_joint=6, jog_dir=0, holding=False):
+
+        req = self.generate_robot_request(
+            cmd_mode=RobotCommand.Request.JOG,
+            jog_joint=jog_joint,
+            jog_dir=jog_dir,
+            holding=holding
+        )
+        return self.call_hiwin(req)
 
     def call_modbus_close(self):
 
-        self.command_req.mode  ='Close'
-        print("Modbus Close") 
-        return self.send_command_callback()
+        req = self.generate_robot_request(
+            cmd_mode=RobotCommand.Request.CLOSE,
+        )
+        return self.call_hiwin(req)
+
+    # def start_main_loop_thread(self):
+    #     self.main_loop_thread = Thread(target=self._main_loop)
+    #     self.main_loop_thread.daemon = True
+    #     self.main_loop_thread.start()
 
 def main(args=None):
     PTP_Angle = [0.00, 0.00, 0.00, 0.00, -90.00, 0.00]                 # ANGLE
@@ -109,17 +228,20 @@ def main(args=None):
     rclpy.init(args=args)
 
     hiwinmodbus_client = HiwinmodbusClient()
+    # hiwinmodbus_client.start_main_loop_thread()
 
-    hiwinmodbus_client.call_motor_excite()
+    # hiwinmodbus_client.call_motor_excite()
     
-    hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle)
-    hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle3)
-    hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle)
-    hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle2, holding= True)
-    hiwinmodbus_client.call_lin(1,200,10,OBJECT_POSE, holding= True)
+    res =hiwinmodbus_client.call_ptp(PTP_Angle, RobotCommand.Request.JOINTS_CMD)
+    print("pppppppp")
+    print(res)
+    hiwinmodbus_client.call_ptp(PTP_Angle3, RobotCommand.Request.JOINTS_CMD)
+    hiwinmodbus_client.call_ptp(PTP_Angle, RobotCommand.Request.JOINTS_CMD)
+    hiwinmodbus_client.call_ptp(PTP_Angle2, RobotCommand.Request.JOINTS_CMD, True)
+    hiwinmodbus_client.call_ptp(OBJECT_POSE, RobotCommand.Request.POSE_CMD, True)
 
-    hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle3)
-    hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle, holding= True)
+    # hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle3)
+    # hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle, holding= True)
     # hiwinmodbus_client.call_HOME()
     hiwinmodbus_client.call_modbus_close()
 
---
+++ hiwin_service_client/scripts/Hiwin_service_client_with_Hiwinmodbus_srv.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+
+
+import rclpy
+from rclpy.node import Node
+
+from hiwin_interfaces.srv import Hiwinmodbus
+'''
+    DO(int DO_Num, int x)                                                         # 1 -> on ; 0 -> off                                          
+    HOME(int state)                                                               # 1 RUN
+    PTP(int type, int vel, int acc, int TOOL, int BASE, double *Angle)            # 0 -> joint ; 1 -> coordinate
+    LIN(int type,double *XYZ, int vel, int acc, int TOOL, int BASE)               # 0 -> joint ; 1 -> coordinate
+    CIRC(double *CIRC_s, double *CIRC_end, int vel, int acc, int TOOL, int BASE) 
+    JOG(int joint,int dir)
+'''
+
+
+class HiwinmodbusClient(Node):
+
+    def __init__(self):
+        super().__init__('hiwinmodbus_client')
+        self.cli = self.create_client(Hiwinmodbus, 'hiwinmodbus_service')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.command_req = Hiwinmodbus.Request()
+
+    def send_command_callback(self):
+        self.future = self.cli.call_async(self.command_req)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+    
+    # def Holding_command(self, holding):
+
+    #     self.command_req.holding = holding
+
+    def call_motor_excite(self, holding=False):
+
+        self.command_req.mode  ='Excite'
+        self.command_req.holding = holding
+        return self.send_command_callback() 
+
+    def call_ptp(self, type, vel, acc, pose, holding=False):
+        
+        self.command_req.mode  ='PTP'
+        self.command_req.type  = type 
+        self.command_req.vel   = vel
+        self.command_req.acc   = acc
+        self.command_req.pose = pose
+        self.command_req.holding = holding
+        return self.send_command_callback()
+
+    def call_line(self, type, vel, acc, pose, holding=False):
+
+        self.command_req.mode  ='LIN'
+        self.command_req.type = type 
+        self.command_req.vel  = vel
+        self.command_req.acc  = acc
+        self.command_req.pose  = pose
+        self.command_req.holding = holding
+        return self.send_command_callback()
+
+
+    def call_circ(self, vel, acc, circ_s, circ_end, holding=False):
+
+        self.command_req.mode  ='CIRC'
+        self.command_req.vel      = vel
+        self.command_req.acc      = acc
+        self.command_req.circ_s   = circ_s
+        self.command_req.circ_end = circ_end
+        self.command_req.holding = holding
+        return self.send_command_callback()
+
+
+    def call_do(self, digital_output, onoff, holding=False):
+
+        self.command_req.mode  ='DO'
+        self.command_req.digital_output = digital_output
+        self.command_req.onoff          = onoff
+        self.command_req.holding = holding
+        return self.send_command_callback()
+
+
+    def call_home(self, holding=False):
+        self.command_req.mode  ='HOME'
+        self.command_req.type = 0
+        self.command_req.holding = holding
+        return self.send_command_callback()
+
+
+    def call_jog(self, joint, dir, holding=False):
+
+        self.command_req.mode  ='JOG'
+        self.command_req.joint = joint
+        self.command_req.dir   = dir
+        self.command_req.holding = holding
+        return self.send_command_callback()
+
+    def call_modbus_close(self):
+
+        self.command_req.mode  ='Close'
+        print("Modbus Close") 
+        return self.send_command_callback()
+
+def main(args=None):
+    PTP_Angle = [0.00, 0.00, 0.00, 0.00, -90.00, 0.00]                 # ANGLE
+    PTP_Angle2 = [20.00, 0.00, 0.00, 0.00, -90.00, 0.00]  
+    PTP_Angle3 = [-20.00, 0.00, 0.00, 0.00, -90.00, 0.00]
+    OBJECT_POSE = [-67.517, 361.753, 293.500, 180.00, 0.00, 100.572]
+    rclpy.init(args=args)
+
+    hiwinmodbus_client = HiwinmodbusClient()
+
+    hiwinmodbus_client.call_motor_excite()
+    
+    hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle)
+    hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle3)
+    hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle)
+    hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle2, holding= True)
+    hiwinmodbus_client.call_ptp(1,200,10,OBJECT_POSE, holding= True)
+
+    # hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle3)
+    # hiwinmodbus_client.call_ptp(0,200,10,PTP_Angle, holding= True)
+    # hiwinmodbus_client.call_HOME()
+    hiwinmodbus_client.call_modbus_close()
+
+
+    hiwinmodbus_client.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
+     
+
+    
+
